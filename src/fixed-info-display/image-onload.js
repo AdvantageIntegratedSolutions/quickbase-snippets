ee915be19b2c9a64aade4f88049379f39b5878d1
Kit Hensel <kithensel@gmail.com>
2017-01-04 16:25:28 -0800

    production git url

--- src/fixed-info-display/image-onload.js
+++ src/fixed-info-display/image-onload.js
@@ -1,2 +1,2 @@
-var text url = "https://rawgit.com/KitHensel/quickbase-snippets/master/src/fixed-info-display/index.js";
+var text url = "https://cdn.rawgit.com/KitHensel/quickbase-snippets/master/src/fixed-info-display/index.js";
 "<img qbu='module' src='/i/clear2x2.gif' onload=\"javascript:$.getScript('" & $url & "')\">"
