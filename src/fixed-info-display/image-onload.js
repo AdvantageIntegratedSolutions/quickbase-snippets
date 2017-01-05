1a2829f22d5e8e200bbfabcb8c0b7415ad4943f3
Kit Hensel <kithensel@gmail.com>
2017-01-04 16:22:37 -0800

    .

--- src/fixed-info-display/image-onload.js
+++ src/fixed-info-display/image-onload.js
@@ -1,3 +1,2 @@
-"<img qbu=\"module\" src=\"/i/clear2x2.gif\" " &
-"onload=\"javascript:if(typeof QBU=='undefined'){QBU={};$.getScript('" &
-'https://raw.githubusercontent.com/KitHensel/quickbase-snippets/master/src/fixed-info-display/index.js' &rand='+Math.random())}\">"
+var text url = "https://rawgit.com/KitHensel/quickbase-snippets/master/src/fixed-info-display/index.js";
+"<img qbu='module' src='/i/clear2x2.gif' onload=\"javascript:$.getScript('" & $url & "')\">"
