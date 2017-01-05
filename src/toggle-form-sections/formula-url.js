be1dfbaec64d9480c5574fb89ff750b6f5f12b0e
Kit Hensel <kithensel@gmail.com>
2017-01-05 10:27:04 -0800

    .

--- src/toggle-form-sections/formula-url.js
+++ src/toggle-form-sections/formula-url.js
@@ -1,1 +1,1 @@
-"javascript:void($.getScript('https://raw.githubusercontent.com/KitHensel/quickbase-snippets/master/src/toggle-form-sections.js'))"
+"javascript:void($.getScript('https://raw.githubusercontent.com/KitHensel/quickbase-snippets/master/src/toggle-form-sections/index.js'))"
